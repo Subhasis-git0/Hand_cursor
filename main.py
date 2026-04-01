22fc195ffd6ac65038be045c6856d8be7df6fda9
Subhasis Mallick <subhasismallick04@gmail.com>
2026-04-01 12:50:18 +0530

    updated code, included drag feature

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ while True:
             
             thumb = hand_landmarks[4]
             index_finger = hand_landmarks[8]
+            middle_finger = hand_landmarks[12]
 
             h, w, _ = frame.shape
             cx = int(index_finger.x * w)
@@ -53,37 +54,36 @@ while True:
 
             thumb_x, thumb_y = int(thumb.x * w), int(thumb.y * h)
             index_x, index_y = int(index_finger.x * w), int(index_finger.y * h)
+            middle_x, middle_y = int(middle_finger.x * w), int(middle_finger.y * h)
 
             distance = math.hypot(index_x - thumb_x, index_y - thumb_y)
+            distance_middle = math.hypot(middle_x - thumb_x, middle_y - thumb_y)
 
             current_time = time.time()
-            if distance < 30:
-                if not pinch_active:
-                    pinch_start_time = current_time
-                    pinch_active = True
-                
-                elif current_time - pinch_start_time > 0.5:
-                    if not dragging:
-                        pyautogui.mouseDown()
-                        dragging = True
+            if distance < 30 and distance_middle > 50:
+                pyautogui.click()
+                time.sleep(0.2)
+            
+            elif distance < 50 and distance_middle < 50:
+                if not dragging:
+                    pyautogui.mouseDown()
+                    dragging = True
+
             else:
-                if pinch_active:
-                    if not dragging:
-                        pyautogui
-                    else:
-                        pyautogui.mouseUp()
-                        dragging = False
-                    pinch_active = False
+                if dragging:
+                    pyautogui.mouseUp()
+                    dragging = False
 
 
-            #map camera coords to screen coords; 1.2 is an optional scaling factor to make the cursor movement more responsive
-            screen_x = int(index_finger.x * screen_w * 1.2)
-            screen_y = int(index_finger.y * screen_h * 1.2)
+            #map camera coords to screen coords; use 1.0 scale for direct mapping
+            screen_x = int(index_finger.x * screen_w * 1.5)
+            screen_y = int(index_finger.y * screen_h * 1.5)
 
             screen_x = max(0, min(screen_w - 1, screen_x))
             screen_y = max(0, min(screen_h - 1, screen_y))
+            
+            smoothening = 5
 
-            smoothening = 6
             curr_x = prev_x + (screen_x - prev_x) / smoothening
             curr_y = prev_y + (screen_y - prev_y) / smoothening
 
@@ -95,6 +95,7 @@ while True:
             prev_x, prev_y = curr_x, curr_y
 
     cv2.imshow("camera", frame)
+    
 
     if cv2.waitKey(1) == 27:
         break
